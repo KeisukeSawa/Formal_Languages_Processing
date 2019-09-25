40829126c74eb92a055b3838163a64590b6dad49
KeisukeSawa <sawa.window@outlook.jp>
2019-09-25 16:03:34 +0900

    Add Interpreter.java

---
+++ 演習3-2/interpreter.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Interpreter{
+
+    // 事前に決められている演算子などの語を配列にまとめる
+    // 予約語配列
+    String[] reserved_word = {"input","print","end","print"};
+    // 演算子配列
+    String[] operator = {"+","-","*","/","="};
+
+    
+
+
+    public static void main(String[] args){
+
+        // 文字の読み込みのために、Scannerクラスのインスタンス生成
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+
+}
